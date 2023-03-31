704b3062699ee70dd955fbb4862e6c6874d13144
zafferano.g <zafferano.g@edificarseguros.com.ar>
2023-03-30 22:26:53 -0300

    Ejercicios Clase 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/usuario.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+export class usuario {
+    nombre : string = '';
+    clave : string = '';
+}
