11434851e364d2e5f83f66160043716a8fd70357
zafferano.g <zafferano.g@edificarseguros.com.ar>
2023-04-07 23:04:28 -0300

    Tarea Clase 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
 
-
+const appMisRutas:Routes= [
+  {path:'bienvenido',component: BienvenidoComponent},//http://localhost:4200/bienvenido
+  {path:'login',component: LoginComponent}, //http://localhost:4200/login
+  {path:'**',component: ErrorComponent} //http://localhost:4200/ Cualquier ruta que no haya matcheado con las anteriores.
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(appMisRutas),
   ]
 })
 export class AppRoutingModule { }
